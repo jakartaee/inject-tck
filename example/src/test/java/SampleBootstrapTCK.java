0f1c62b813d438a8ec508aa92d1b2eb669af3d38
Scott M Stark <starksm64@gmail.com>
2019-07-25 22:28:38 -0700

    Update to use 3.1.1.Final and the standard SE bootstrap. Exclude discovery of DriversSeat and SpareTire beans.
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- example/src/test/java/SampleBootstrapTCK.java
+++ example/src/test/java/SampleBootstrapTCK.java
@@ -1,29 +1,16 @@
 import junit.framework.Test;
 import org.atinject.tck.Tck;
 import org.atinject.tck.auto.Car;
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
 
-//import javax.enterprise.inject.se.SeContainer;
-//import javax.enterprise.inject.se.SeContainerInitializer;
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
 
 public class SampleBootstrapTCK {
 
     public static Test suite() {
 
-        /*
         SeContainer container = SeContainerInitializer.newInstance().initialize();
-        //container.getBeanManager().createAnnotatedType()
-
         Car tckCar = container.select(Car.class).get();
-
-         */
-        Weld weld = new Weld();
-
-        WeldContainer container = weld.initialize();
-
-        Car tckCar = container.select(Car.class).get();
-
         return Tck.testsFor(tckCar, false, true);
     }
 }
