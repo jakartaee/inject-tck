9c5ffa2920d5448a7d11721abf94e8afa9c45bd7
Scott M Stark <starksm64@gmail.com>
2019-07-25 22:04:42 -0700

    Use weld 2.4.8.Final
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- example/src/test/java/SampleBootstrapTCK.java
+++ example/src/test/java/SampleBootstrapTCK.java
@@ -1,16 +1,26 @@
 import junit.framework.Test;
 import org.atinject.tck.Tck;
 import org.atinject.tck.auto.Car;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
 
-import javax.enterprise.inject.se.SeContainer;
-import javax.enterprise.inject.se.SeContainerInitializer;
+//import javax.enterprise.inject.se.SeContainer;
+//import javax.enterprise.inject.se.SeContainerInitializer;
 
 public class SampleBootstrapTCK {
 
     public static Test suite() {
 
+        /*
         SeContainer container = SeContainerInitializer.newInstance().initialize();
         //container.getBeanManager().createAnnotatedType()
+
+        Car tckCar = container.select(Car.class).get();
+
+         */
+        Weld weld = new Weld();
+
+        WeldContainer container = weld.initialize();
 
         Car tckCar = container.select(Car.class).get();
 
